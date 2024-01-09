fbff9d802ba37f626895265020ef16b6f8779fb5
RajrupDasid <rajrupofficialid@protonmail.com>
2024-01-09 19:25:55 +0530

    project settings
    ELAD-4
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app.config.js
+++ app.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   expo: {
-    scheme: "myapp",
+    scheme: "E_learning_management",
     extra: {
       clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
       masterurl: process.env.HYGRAPH_API_URL,
+      eas: {
+        projectId: process.env.PROJECTID,
+      },
+    },
+    android: {
+      package: "com.webstackpros.elearningapp",
     },
   },
 };
