5306a7004bc04a3d4460ef343287c8df5454cb3b
RajrupDasid <rajrupofficialid@protonmail.com>
2024-01-09 19:18:49 +0530

    Subheading
    Subheading sections are sections which will be shown in home page and other section
    
    ELAD-4
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- App/Components/HomeScreen/SubHeading.js
+++ App/Components/HomeScreen/SubHeading.js
@@ -1,15 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Colors from "../../Utils/Colors";
 
-export default function SubHeading() {
+export default function SubHeading({ text, color = color }) {
   return (
     <View>
       <Text
         style={{
           fontSize: 18,
           fontWeight: "bold",
+          color: color,
         }}>
-        Basic Courses
+        {text}
       </Text>
     </View>
   );
