b38658448130515546fd78f6cb9297050e4faafd
RajrupDasid <rajrupofficialid@protonmail.com>
2024-01-09 19:14:27 +0530

    Course List or course display area section
    ELAD-4
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- App/Components/HomeScreen/CourseList.js
+++ App/Components/HomeScreen/CourseList.js
@@ -1,6 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React, { useEffect, useState } from "react";
 import { getCourseList } from "../../Services";
+import SubHeading from "./SubHeading";
+import Colors from "../../Utils/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function CourseList({ level }) {
   const [CourseList, setCourse] = useState([]);
@@ -9,9 +12,90 @@ export default function CourseList({ level }) {
   }, []);
   const getCourses = () => {
     getCourseList(level).then((resp) => {
-      console.log("Response", resp);
       setCourse(resp?.courses);
     });
   };
-  return <View></View>;
+  return (
+    <View>
+      <SubHeading
+        text={level + "Courses"}
+        color={level == "Basic" && Colors.WHITE}
+      />
+      <FlatList
+        data={CourseList}
+        key={CourseList.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              padding: 10,
+              backgroundColor: Colors.WHITE,
+              marginRight: 15,
+              borderRadius: 20,
+            }}>
+            <Image
+              source={{ uri: item?.banner?.url }}
+              style={{ width: 210, height: 120, borderRadius: 15 }}
+            />
+            <View style={{ padding: 7 }}>
+              <Text style={{ fontSize: 17 }}>{item.name}</Text>
+              {/* meta data */}
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}>
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 5,
+                    marginTop: 5,
+                  }}>
+                  <Ionicons name="book-outline" size={24} color="black" />
+                  <Text
+                    style={{
+                      color: Colors.BLACK,
+                      fontWeight: "700",
+                    }}>
+                    {item?.chapters?.length} Chapters
+                  </Text>
+                </View>
+                {/* another view */}
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 5,
+                    marginTop: 5,
+                  }}>
+                  <Ionicons name="md-time-outline" size={24} color="black" />
+                  <Text
+                    style={{
+                      marginTop: 5,
+                      color: Colors.BLACK,
+                      fontWeight: "900",
+                    }}>
+                    {item?.time} Time
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  marginTop: 5,
+                  color: Colors.PRIMARY,
+                  fontWeight: "900",
+                }}>
+                {item.price == 0 ? "Free" : item.price}
+              </Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
